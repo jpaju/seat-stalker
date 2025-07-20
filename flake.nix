a1499c664fa4ae330b36004ecb45b9de4aba0134
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-07-20 22:13:09 +0200

    Add nix flake for dev environment

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "General flake for Scala development";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "scala";
+          packages = with pkgs; [ # Newline
+            sbt
+            temurin-bin-21
+            scala-cli
+            azure-functions-core-tools
+          ];
+
+          shellHook = ''
+            export SBT_TPOLECAT_DEV=1
+          '';
+        };
+      });
+}
