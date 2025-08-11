db48c437ee1c6122b614f53e3d68d39222412525
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-08-11 19:03:57 +0300

    Show terraform plan in GH actions UI

--- flake.nix
+++ flake.nix
@@ -4,28 +4,39 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      in {
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
         devShells.default = pkgs.mkShell {
           name = "scala";
-          packages = with pkgs; [ # Newline
+          packages = with pkgs; [
             sbt
-            temurin-bin-21
             scala-cli
-            azure-cli
+            temurin-bin-21
             azure-functions-core-tools
-            terraform
-            tfsec
           ];
 
           shellHook = ''
             export SBT_TPOLECAT_DEV=1
           '';
         };
-      });
+
+        devShells.infrastructure = pkgs.mkShell {
+          name = "infrastructure";
+          packages = with pkgs; [
+            azure-cli
+            terraform
+            tfsec
+          ];
+        };
+      }
+    );
 }
